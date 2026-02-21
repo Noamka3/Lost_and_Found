238beacf648533ecb544feb73efc517ce032712f
Noamka3 <Noamkadosh4444@gmail.com>
2026-02-21 13:47:33 +0200

    add POst /(add post User}

--- antigravity/src/App.jsx
+++ antigravity/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import ItemDetailPage from "./pages/ItemDetailPage";
 import { Routes, Route } from "react-router-dom";
+import PostItemPage from "./pages/PostItemPage";
 
 export default function App() {
   return (
@@ -12,6 +13,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/item/:id" element={<ItemDetailPage />} />
+
+        <Route path="/post" element={
+            <PostItemPage />
+        } />
+
         <Route path="*" element={<div style={{ padding: 24 }}>404</div>} />
       </Routes>
     </>
